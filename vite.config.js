06be1a862fbaa5b053c4b08526b02b3c7ccce231
Pranav-2812 <pranavdhumale2812@gmail.com>
2024-12-25 01:01:36 +0530

    Modified Package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import {tailwindcss} from "tailwindcss"
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "tailwindcss",
+      config: {
+        config: "./tailwind.config.js",
+      },
+    },
+  ],
+});
