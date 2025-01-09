5d50eeb86f359bd7b03049747699969f82f16828
Gajanan Palepwad <2021bcs041@sggs.ac.in>
2025-01-10 03:31:13 +0530

    done with timetable and home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
+import CountdownTimer from "../components/CountdownTimer";
 
 function Home() {
   return (
     <>
+
+
+      {/* Background Image with Opacity */}
+      <div
+        className="fixed inset-0 z-50"
+        style={{
+          backgroundImage: "url('/master-mask.png')",
+          backgroundSize: '45%',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          opacity: '0.15'
+        }}
+      />
+
+
       {/* Main Banner Section */}
       <div
 
-  className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4  text-center  w-full md:px-8 sm:flex "
-  // style={{ backgroundImage: "url('./bgcs.jpg')" }}
-  >
-    <img
-              src="/mask.png"
-              alt="GDG Logo"
-              className="h-40 absolute top-20 left-40"
-            />
-            <img
-              src="/theboss.png"
-              alt="GDG Logo"
-              className="h-60 absolute  bottom-20 left-20"
-            />
+        className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4  text-center  w-full md:px-8 sm:flex "
+      // style={{ backgroundImage: "url('./bgcs.jpg')" }}
+      >
+        <img
+          src="/mask.png"
+          alt="GDG Logo"
+          className="h-40 absolute top-20 left-40"
+        />
+        <img
+          src="/theboss.png"
+          alt="GDG Logo"
+          className="h-60 absolute  bottom-20 left-20"
+        />
         <div className="p-8 text-center max-w-3xl w-full mx-4 sm:mx-8">
           {/* Logo */}
           <div className="flex items-center justify-center mb-4">
             <img
               src="/logon.png"
-              className="h-12 w-80"
+              className="h-12 w-80 md:h-auto md:w-[900px]"
               alt="Logo"
             />
-        
+
           </div>
           {/* Subtitle */}
 
@@ -39,8 +55,10 @@ function Home() {
           {/* Registration Button */}
 
           <button className="bg-pink-500 py-2 px-6 rounded-full shadow-lg hover:bg-pink-600 transition duration-300 font-squid cursor-pointer">
-            Registrations will start from 2025, 10th Jan *
+            Register Now
           </button>
+
+          <CountdownTimer targetDate="2025-02-10T00:00:00" />
 
           {/* Logos Section */}
           <div className="flex items-center justify-center gap-4 pt-8 flex-wrap">
@@ -58,29 +76,21 @@ function Home() {
         </div>
 
         <img
-              src="/gaurd.png"
-              alt="GDG Logo"
-              className="h-60 absolute  bottom-20 right-20"
-            />
-              
-              <div className="fixed top-4 right-4 group">
-  {/* Image */}
-  <img
-    src="/master-1.png"
-    alt="GDG Logo"
-    className="w-24 h-24 transition-transform duration-300 group-hover:scale-110 group-hover:rotate-6"
-  />
+          src="/gaurd.png"
+          alt="GDG Logo"
+          className="h-60 absolute  bottom-20 right-20"
+        />
 
-  {/* Tooltip Text */}
-  <div className="absolute top-20 right-0 bg-gray-800 text-white text-sm py-2 px-4 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-    get ready for it
-  </div>
-</div>
+        <div className="fixed top-4 right-4 group">
 
-      
+
+
         </div>
 
-      
+
+      </div>
+
+
     </>
   );
 }
